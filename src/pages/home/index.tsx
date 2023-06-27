40800d62d50239f2cb13536766fd8933a0b6ca5f
Denis Moreira <silvad281@gmail.com>
2023-06-27 16:19:36 -0300

    feat: criando tabela e estilizando

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,5 +4,44 @@ import { BiSearch } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
 export function Home() {
-   return <h1>home</h1>;
+   return (
+      <main className={styles.container}>
+         <form className={styles.form}>
+            <input placeholder="Digite o simbolo da moeda: BTC.." />
+            <button type="submit">
+               <BiSearch size={30} color="#FFF" />
+            </button>
+         </form>
+
+         <table>
+            <thead>
+               <th scope="col">Moeda</th>
+               <th scope="col">Valor mercado</th>
+               <th scope="col">Preço</th>
+               <th scope="col">Volume</th>
+            </thead>
+
+            <tbody id="tbody">
+               <tr className={styles.tr}>
+                  <td className={styles.td} data-label="Moeda">
+                     <Link className={styles.link} to="/detail/btc">
+                        <span>Bitcoin</span> | BTC
+                     </Link>
+                  </td>
+
+                  <td className={styles.tdLabel} data-label="Valor de mercado">
+                     R$ 409623
+                  </td>
+
+                  <td className={styles.tdLabel} data-label="Preço">
+                     R$ 409623
+                  </td>
+                  <td className={styles.tdProfit} data-label="Volume">
+                     <span>-5.3</span>
+                  </td>
+               </tr>
+            </tbody>
+         </table>
+      </main>
+   );
 }
