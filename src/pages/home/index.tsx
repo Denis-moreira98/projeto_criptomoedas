8c8d7ab47cd89a36594a5758b8cffdfb351a6abf
Denis Moreira <silvad281@gmail.com>
2023-06-29 20:54:45 -0300

    feat: conectando api (Coinlib)

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,60 @@
 import styles from "./home.module.css";
+import { useEffect, useState } from "react";
 
 import { BiSearch } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
+//https://sujeitoprogramador.com/api-cripto/?key=60d3226e55eae848
+
+interface CoinProps {
+   name: string;
+   delta_24h: string;
+   symbol: string;
+   volume_24h: string;
+   market_cap: string;
+   price: string;
+   formatedPrice: string;
+   formatedMarket: string;
+}
+
+interface DataProps {
+   coins: CoinProps[];
+}
+
 export function Home() {
+   const [coins, setCoins] = useState<CoinProps[]>([]);
+
+   useEffect(() => {
+      function getData() {
+         fetch(
+            "https://sujeitoprogramador.com/api-cripto/?key=60d3226e55eae848"
+         )
+            .then((response) => response.json())
+            .then((data: DataProps) => {
+               // a resquisição deu tudo certo
+               const coinsData = data.coins.slice(0, 15);
+
+               const price = Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+               });
+
+               const formartResult = coinsData.map((item) => {
+                  const formated = {
+                     ...item,
+                     formatedPrice: price.format(Number(item.price)),
+                     formatedmarket: price.format(Number(item.market_cap)),
+                  };
+
+                  return formated;
+               });
+               //console.log(formartResult);
+               setCoins(formartResult);
+            });
+      }
+      getData();
+   }, []);
+
    return (
       <main className={styles.container}>
          <form className={styles.form}>
